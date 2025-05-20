77e6dce9173b3f4d8f244fb91a480e7fc49b992e
Baturhan Çağatay <101994603+BaturhanCagatay@users.noreply.github.com>
2025-05-20 22:45:16 +0300

    Final Version Of FastFlow

---
+++ FastFlow/dataset.py
@@ -0,0 +1,68 @@
+import os
+from glob import glob
+
+import torch
+import torch.utils.data
+from PIL import Image
+from torchvision import transforms
+
+
+class MVTecDataset(torch.utils.data.Dataset):
+    def __init__(self, root, category, input_size, is_train=True):
+        self.image_transform = transforms.Compose(
+            [
+                transforms.Resize((input_size, input_size)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        )
+        if is_train:
+            self.image_files = glob(
+                os.path.join(root, category, "train", "good", "*.jpg")
+            )
+        else:
+            self.image_files = glob(os.path.join(root, category, "test", "*", "*.jpg"))
+            self.target_transform = transforms.Compose(
+                [
+                    transforms.Resize((input_size, input_size)),
+                    transforms.ToTensor(),
+                ]
+            )
+        self.is_train = is_train
+
+    def __getitem__(self, index):
+        image_file = self.image_files[index]
+
+        # Görseli oku ve 3 kanala dönüştür
+        image = Image.open(image_file).convert("RGB")
+        image = self.image_transform(image)
+
+        if self.is_train:
+            return image
+        else:
+            # GOOD örneklerde maske yok: tüm sıfırlardan oluşan maske
+            if os.path.dirname(image_file).endswith("good"):
+                target = torch.zeros([1, image.shape[-2], image.shape[-1]])
+            else:
+                # Maske dosya yolunu üret
+                mask_path = (
+                    image_file.replace("/test/", "/ground/")
+                    .replace(".jpg", ".jpg")
+                    .replace(".jpeg", ".jpg")
+                    .replace(".png", ".jpg")
+                )
+                if not os.path.exists(mask_path):
+                    print(f"❌ Maske dosyası bulunamadı: {mask_path}")
+                    target = torch.zeros([1, image.shape[-2], image.shape[-1]])
+                else:
+                    mask = Image.open(mask_path).convert("L")
+                    target = self.target_transform(mask)
+            target = (target > 0.7).float()
+            return image, target
+
+    def __len__(self):
+        return len(self.image_files)
+
+
+    def __len__(self):
+        return len(self.image_files)
